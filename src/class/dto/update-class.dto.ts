ae704ccb9e567f4a3bbbd6ee3c9618e4e0a3a5f1
124tranvita <124tranvita@gmail.com>
2023-06-09 06:31:27 +0000

    fix update-class.dto.ts

--- src/class/dto/update-class.dto.ts
+++ src/class/dto/update-class.dto.ts
@@ -1,3 +1,4 @@
+import { PartialType } from '@nestjs/swagger';
 import { BaseClassDto } from './base-class.dto';
 
-export class UpdateClassDto extends BaseClassDto {}
+export class UpdateClassDto extends PartialType(BaseClassDto) {}
